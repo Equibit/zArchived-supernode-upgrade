925d2cc8dd0f3453160dfa7cbb261cd0d75ffec2
David Cauz <cauz.david@gmail.com>
2016-10-07 21:08:27 -0400

    Patch  ecd7fd37c888f8ebc64cf3d92272975b37ae54ca

--- src/edc/edcmain.cpp
+++ src/edc/edcmain.cpp
@@ -5472,7 +5472,7 @@ bool ProcessMessage(
         {
             boost::this_thread::interruption_point();
 
-            if (!(addr.nServices & NODE_NETWORK))
+			if ((addr.nServices & REQUIRED_SERVICES) != REQUIRED_SERVICES)
                 continue;
 
             if (addr.nTime <= 100000000 || addr.nTime > nNow + 10 * 60)
