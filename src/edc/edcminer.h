9a8782695c93d03dea3fdb0aa5c8058696b065ca
David Cauz <cauz.david@gmail.com>
2016-10-09 11:22:57 -0400

    Patch  c82a4e9a63a28fc8c482c7c8e5b7bfcc51a6805a

--- src/edc/edcminer.h
+++ src/edc/edcminer.h
@@ -11,6 +11,8 @@
 
 #include <stdint.h>
 #include <memory>
+#include "boost/multi_index_container.hpp"
+#include "boost/multi_index/ordered_index.hpp"
 
 class CBlockIndex;
 class CEDCChainParams;
@@ -27,6 +29,109 @@ struct CEDCBlockTemplate
     CEDCBlock block;
     std::vector<CAmount> vTxFees;
     std::vector<int64_t> vTxSigOps;
+};
+
+// Container for tracking updates to ancestor feerate as we include (parent)
+// transactions in a block
+struct CEDCTxMemPoolModifiedEntry 
+{
+    CEDCTxMemPoolModifiedEntry(CEDCTxMemPool::txiter entry)
+    {
+        iter = entry;
+        nSizeWithAncestors = entry->GetSizeWithAncestors();
+        nModFeesWithAncestors = entry->GetModFeesWithAncestors();
+        nSigOpCountWithAncestors = entry->GetSigOpCountWithAncestors();
+    }
+
+    CEDCTxMemPool::txiter iter;
+    uint64_t nSizeWithAncestors;
+    CAmount nModFeesWithAncestors;
+    unsigned int nSigOpCountWithAncestors;
+};
+
+/** Comparator for CTxMemPool::txiter objects.
+ *  It simply compares the internal memory address of the CTxMemPoolEntry object
+ *  pointed to. This means it has no meaning, and is only useful for using them
+ *  as key in other indexes.
+ */
+struct EDCCompareCTxMemPoolIter 
+{
+    bool operator()(const CEDCTxMemPool::txiter& a, const CEDCTxMemPool::txiter& b) const
+    {
+        return &(*a) < &(*b);
+    }
+};
+
+struct EDCmodifiedentry_iter 
+{
+    typedef CEDCTxMemPool::txiter result_type;
+    result_type operator() (const CEDCTxMemPoolModifiedEntry &entry) const
+    {
+        return entry.iter;
+    }
+};
+
+// This matches the calculation in CompareTxMemPoolEntryByAncestorFee,
+// except operating on CTxMemPoolModifiedEntry.
+// TODO: refactor to avoid duplication of this logic.
+struct EDCCompareModifiedEntry 
+{
+    bool operator()(const CEDCTxMemPoolModifiedEntry &a, const CEDCTxMemPoolModifiedEntry &b)
+    {
+        double f1 = (double)a.nModFeesWithAncestors * b.nSizeWithAncestors;
+        double f2 = (double)b.nModFeesWithAncestors * a.nSizeWithAncestors;
+        if (f1 == f2) {
+            return CEDCTxMemPool::CompareIteratorByHash()(a.iter, b.iter);
+        }
+        return f1 > f2;
+    }
+};
+
+// A comparator that sorts transactions based on number of ancestors.
+// This is sufficient to sort an ancestor package in an order that is valid
+// to appear in a block.
+struct EDCCompareTxIterByAncestorCount 
+{
+    bool operator()(const CEDCTxMemPool::txiter &a, const CEDCTxMemPool::txiter &b)
+    {
+        if (a->GetCountWithAncestors() != b->GetCountWithAncestors())
+            return a->GetCountWithAncestors() < b->GetCountWithAncestors();
+        return CEDCTxMemPool::CompareIteratorByHash()(a, b);
+    }
+};
+
+typedef boost::multi_index_container<
+    CEDCTxMemPoolModifiedEntry,
+    boost::multi_index::indexed_by<
+        boost::multi_index::ordered_unique<
+            EDCmodifiedentry_iter,
+            EDCCompareCTxMemPoolIter
+        >,
+        // sorted by modified ancestor fee rate
+        boost::multi_index::ordered_non_unique<
+            // Reuse same tag from CTxMemPool's similar index
+            boost::multi_index::tag<ancestor_score>,
+            boost::multi_index::identity<CEDCTxMemPoolModifiedEntry>,
+            EDCCompareModifiedEntry
+        >
+    >
+> EDCindexed_modified_transaction_set;
+
+typedef EDCindexed_modified_transaction_set::nth_index<0>::type::iterator EDCmodtxiter;
+typedef EDCindexed_modified_transaction_set::index<ancestor_score>::type::iterator EDCmodtxscoreiter;
+
+struct EDCupdate_for_parent_inclusion
+{
+    EDCupdate_for_parent_inclusion(CEDCTxMemPool::txiter it) : iter(it) {}
+
+    void operator() (CEDCTxMemPoolModifiedEntry &e)
+    {
+        e.nModFeesWithAncestors -= iter->GetFee();
+        e.nSizeWithAncestors -= iter->GetTxSize();
+        e.nSigOpCountWithAncestors -= iter->GetSigOpCount();
+    }
+
+    CEDCTxMemPool::txiter iter;
 };
 
 /** Generate a new block, without valid proof-of-work */
@@ -81,6 +186,9 @@ private:
     /** Add transactions based on tx "priority" */
     void addPriorityTxs();
 
+    /** Add transactions based on feerate including unconfirmed ancestors */
+    void addPackageTxs();
+
     // helper function for addScoreTxs and addPriorityTxs
 
     /** Test if tx will still "fit" in the block */
@@ -88,6 +196,27 @@ private:
 
     /** Test if tx still has unconfirmed parents not yet in block */
     bool isStillDependent(CEDCTxMemPool::txiter iter);
+
+    // helper functions for addPackageTxs()
+    /** Remove confirmed (inBlock) entries from given set */
+    void onlyUnconfirmed(CEDCTxMemPool::setEntries& testSet);
+
+    /** Test if a new package would "fit" in the block */
+    bool TestPackage(uint64_t packageSize, unsigned int packageSigOps);
+
+    /** Test if a set of transactions are all final */
+    bool TestPackageFinality(const CEDCTxMemPool::setEntries& package);
+
+    /** Return true if given transaction from mapTx has already been evaluated,
+      * or if the transaction's cached data in mapTx is incorrect. */
+    bool SkipMapTxEntry(CEDCTxMemPool::txiter it, EDCindexed_modified_transaction_set &mapModifiedTx, CEDCTxMemPool::setEntries &failedTx);
+
+    /** Sort the package in an order that is valid to appear in a block */
+    void SortForBlock(const CEDCTxMemPool::setEntries& package, CEDCTxMemPool::txiter entry, std::vector<CEDCTxMemPool::txiter>& sortedEntries);
+
+    /** Add descendants of given transactions to mapModifiedTx with ancestor
+      * state updated assuming given transactions are inBlock. */
+    void UpdatePackagesForAdded(const CEDCTxMemPool::setEntries& alreadyAdded, EDCindexed_modified_transaction_set &mapModifiedTx);
 };
 
 /** Modify the extranonce in a block */
