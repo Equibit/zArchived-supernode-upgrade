1629f1428e9141a60a1246667e44f157afd14ec3
David Cauz <cauz.david@gmail.com>
2016-10-15 15:29:17 -0400

    Patch f4691ab3a9d4f3321afa024984c03fe6e10bfdbc

--- src/edc/rpc/edcmisc.cpp
+++ src/edc/rpc/edcmisc.cpp
@@ -355,6 +355,44 @@ UniValue edccreatemultisig(const UniValue& params, bool fHelp)
     return result;
 }
 
+UniValue edccreatewitnessaddress(const UniValue& params, bool fHelp)
+{
+    if (fHelp || params.size() < 1 || params.size() > 1)
+    {
+        string msg = "eb_createwitnessaddress \"script\"\n"
+            "\nCreates a witness address for a particular script.\n"
+            "It returns a json object with the address and witness script.\n"
+
+            "\nArguments:\n"
+            "1. \"script\"       (string, required) A hex encoded script\n"
+
+            "\nResult:\n"
+            "{\n"
+            "  \"address\":\"multisigaddress\",  (string) The value of the new address (P2SH of witness script).\n"
+            "  \"witnessScript\":\"script\"      (string) The string value of the hex-encoded witness script.\n"
+            "}\n"
+        ;
+        throw runtime_error(msg);
+    }
+
+    if (!IsHex(params[0].get_str())) 
+	{
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Script must be hex-encoded");
+    }
+
+    std::vector<unsigned char> code = ParseHex(params[0].get_str());
+    CScript script(code.begin(), code.end());
+    CScript witscript = GetScriptForWitness(script);
+    CScriptID witscriptid(witscript);
+    CBitcoinAddress address(witscriptid);
+
+    UniValue result(UniValue::VOBJ);
+    result.push_back(Pair("address", address.ToString()));
+    result.push_back(Pair("witnessScript", HexStr(witscript.begin(), witscript.end())));
+
+    return result;
+}
+
 UniValue edcverifymessage(const UniValue& params, bool fHelp)
 {
     if (fHelp || params.size() != 3)
@@ -491,6 +529,7 @@ static const CRPCCommand edcCommands[] =
     { "control",            "eb_getinfo",                &edcgetinfo,                true  }, /* uses wallet if enabled */
     { "util",               "eb_validateaddress",        &edcvalidateaddress,        true  }, /* uses wallet if enabled */
     { "util",               "eb_createmultisig",         &edccreatemultisig,         true  },
+	{ "util",               "eb_createwitnessaddress",   &edccreatewitnessaddress,   true  },
     { "util",               "eb_verifymessage",          &edcverifymessage,          true  },
     { "util",               "eb_signmessagewithprivkey", &edcsignmessagewithprivkey, true  },
 
