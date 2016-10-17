0d79904058f7acfb277f507c27c3e3a1c593a937
David Cauz <cauz.david@gmail.com>
2016-10-17 08:24:15 -0400

    Patch 20f3cd75f6af8f75de943fef43b4b7769746f732

--- src/edc/wallet/edcwallet.cpp
+++ src/edc/wallet/edcwallet.cpp
@@ -2167,17 +2167,6 @@ static void ApproximateBestSubset(
             }
         }
     }
-
-    // Reduces the approximate best subset by removing any inputs that are 
-	// smaller than the surplus of nTotal beyond nTargetValue. 
-    for (unsigned int i = 0; i < vValue.size(); i++)
-    {                        
-        if (vfBest[i] && (nBest - vValue[i].first) >= nTargetValue )
-        {
-            vfBest[i] = false;
-            nBest -= vValue[i].first;
-        }
-    }
 }
 
 bool CEDCWallet::SelectCoinsMinConf(
