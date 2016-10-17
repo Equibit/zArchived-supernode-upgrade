463f0b640ced81423cf3d199295ba5d3d29b4ca0
David Cauz <cauz.david@gmail.com>
2016-10-17 17:31:58 -0400

    Patch  2266b43e3317a889b9150e614169acda50383bf5

--- src/edc/wallet/edcrpcwallet.cpp
+++ src/edc/wallet/edcrpcwallet.cpp
@@ -2477,7 +2477,8 @@ UniValue edcencryptwallet(const UniValue& params, bool fHelp)
     // slack space in .dat files; that is bad if the old data is
     // unencrypted private keys. So:
     StartShutdown();
-    return "wallet encrypted; Equibit server stopping, restart to run with encrypted wallet. The keypool has been flushed, you need to make a new backup.";
+
+	return "wallet encrypted; Bitcoin server stopping, restart to run with encrypted wallet. The keypool has been flushed and a new HD seed was generated (if you are using HD). You need to make a new backup.";
 }
 
 UniValue edclockunspent(const UniValue& params, bool fHelp)
--- src/edc/wallet/edcwallet.cpp
+++ src/edc/wallet/edcwallet.cpp
@@ -721,6 +721,16 @@ bool CEDCWallet::EncryptWallet(const SecureString& strWalletPassphrase)
 
         Lock();
         Unlock(strWalletPassphrase);
+
+        // if we are using HD, replace the HD master key (seed) with a new one
+        if (!hdChain.masterKeyID.IsNull()) 
+		{
+            CKey key;
+            CPubKey masterPubKey = GenerateNewHDMasterKey();
+            if (!SetHDMasterKey(masterPubKey))
+                return false;
+        }
+
         NewKeyPool();
 #ifdef USE_HSM
         NewHSMKeyPool();
@@ -1324,19 +1334,42 @@ CAmount CEDCWallet::GetChange(const CEDCTransaction& tx) const
     return nChange;
 }
 
-bool CEDCWallet::SetHDMasterKey(const CKey& key)
+CPubKey CEDCWallet::GenerateNewHDMasterKey()
+{
+    CKey key;
+    key.MakeNewKey(true);
+
+    int64_t nCreationTime = GetTime();
+    CKeyMetadata metadata(nCreationTime);
+
+    // calculate the pubkey
+    CPubKey pubkey = key.GetPubKey();
+    assert(key.VerifyPubKey(pubkey));
+
+    // set the hd keypath to "m" -> Master, refers the masterkeyid to itself
+    metadata.hdKeypath     = "m";
+    metadata.hdMasterKeyID = pubkey.GetID();
+
+    {
+        LOCK(cs_wallet);
+
+        // mem store the metadata
+        mapKeyMetadata[pubkey.GetID()] = metadata;
+
+        // write the key&metadata to the database
+        if (!AddKeyPubKey(key, pubkey))
+            throw std::runtime_error("CWallet::GenerateNewKey(): AddKey failed");
+    }
+
+    return pubkey;
+}
+
+bool CEDCWallet::SetHDMasterKey(const CPubKey& pubkey)
 {
     LOCK(cs_wallet);
 
     // ensure this wallet.dat can only be opened by clients supporting HD
     SetMinVersion(FEATURE_HD);
-
-    // store the key as normal "key"/"ckey" object
-    // in the database
-    // key metadata is not required
-    CPubKey pubkey = key.GetPubKey();
-    if (!AddKeyPubKey(key, pubkey))
-        throw std::runtime_error("CWallet::GenerateNewKey(): AddKey failed");
 
     // store the keyid (hash160) together with
     // the child index counter in the database
@@ -4057,8 +4090,8 @@ bool CEDCWallet::InitLoadWallet()
 		{
             // generate a new master key
             CKey key;
-            key.MakeNewKey(true);
-            if (!walletInstance->SetHDMasterKey(key))
+            CPubKey masterPubKey = walletInstance->GenerateNewHDMasterKey();
+            if (!walletInstance->SetHDMasterKey(masterPubKey))
                 throw std::runtime_error("CWallet::GenerateNewKey(): Storing master key failed");
         }
 
